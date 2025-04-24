5b814eef80026c4fe02494f327cf2914e161b7bd
Lee-0210 <tjsans9069@gmail.com>
2025-04-24 15:18:03 +0900

    메인 -> 리스트 구현

--- board/src/main/java/com/team1/Controller/MainController.java
+++ board/src/main/java/com/team1/Controller/MainController.java
@@ -1,4 +1,15 @@
 package com.team1.Controller;
+
+import java.io.IOException;
+import com.team1.Util.SceneUtil;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class MainController {
+
+      @FXML
+      void toList(ActionEvent event) throws IOException {
+        SceneUtil.getInstance().switchScene(event, "/com/team1/UI/List.fxml");
+      }
 
 }
