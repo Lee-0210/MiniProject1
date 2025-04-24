4d5813d1f81e5e3cfd87ffb5333d4d5f57da0676
Lee-0210 <tjsans9069@gmail.com>
2025-04-24 15:35:37 +0900

    메인 -> 리스트 기능 추가

--- board/src/main/java/com/team1/Controller/MainController.java
+++ board/src/main/java/com/team1/Controller/MainController.java
@@ -1,15 +1,32 @@
 package com.team1.Controller;
 
 import java.io.IOException;
+
 import com.team1.Util.SceneUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
 
 public class MainController {
 
+      @FXML private TextField userid;
+      @FXML private TextField userpw;
+
+
+
+
       @FXML
       void toList(ActionEvent event) throws IOException {
+        if(!userid.getText().equals("윤홍민바보") && !userpw.getText().equals("1234")) {
+          Alert alert = new Alert(AlertType.CONFIRMATION);
+          alert.setTitle("알림");
+          alert.setHeaderText("알림");
+          alert.setContentText("아이디: 윤홍민바보 \n 비밀번호 : 1234");
+          alert.showAndWait();
+          return;
+        }
         SceneUtil.getInstance().switchScene(event, "/com/team1/UI/List.fxml");
       }
-
 }
